e0253c19deba8e98ade447622cf0fc76b70210a0
germanrogu <german.rodriguezgu@gmail.com>
2024-02-18 18:02:19 -0500

    use case orthography

---
+++ src/gpt/use-cases/index.ts
@@ -0,0 +1,1 @@
+export * from './orthopgraphy.use-case';
---
+++ src/gpt/use-cases/orthopgraphy.use-case.ts
@@ -0,0 +1,10 @@
+interface Options {
+  prompt: string;
+}
+
+export const orthographyCheckUseCase = async (options: Options) => {
+  const { prompt } = options;
+  return {
+    prompt,
+  };
+};
