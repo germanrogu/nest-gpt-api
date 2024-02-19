a9425448908f7f8fe5fb12ebc8c289e58f72d9cc
germanrogu <german.rodriguezgu@gmail.com>
2024-02-18 19:08:14 -0500

    Use OpenAi

--- src/gpt/use-cases/orthopgraphy.use-case.ts
+++ src/gpt/use-cases/orthopgraphy.use-case.ts
@@ -1,10 +1,27 @@
+import OpenAI from 'openai';
+
 interface Options {
   prompt: string;
 }
 
-export const orthographyCheckUseCase = async (options: Options) => {
+export const orthographyCheckUseCase = async (
+  openai: OpenAI,
+  options: Options,
+) => {
   const { prompt } = options;
+
+  const completion = await openai.chat.completions.create({
+    messages: [
+      { role: 'system', content: 'You are a helpful assistant.' },
+      { role: 'user', content: prompt },
+    ],
+    model: 'gpt-3.5-turbo',
+  });
+
+  console.log(completion);
+
   return {
     prompt,
+    completion,
   };
 };
