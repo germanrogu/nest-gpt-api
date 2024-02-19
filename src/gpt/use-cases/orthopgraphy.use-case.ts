1901c9f0aee649d8c995f526f1e2100338944c35
germanrogu <german.rodriguezgu@gmail.com>
2024-02-18 20:00:40 -0500

    configure OPEN AI

--- src/gpt/use-cases/orthopgraphy.use-case.ts
+++ src/gpt/use-cases/orthopgraphy.use-case.ts
@@ -12,16 +12,28 @@ export const orthographyCheckUseCase = async (
 
   const completion = await openai.chat.completions.create({
     messages: [
-      { role: 'system', content: 'You are a helpful assistant.' },
+      {
+        role: 'system',
+        content: `
+            Recibirás un texto en español con posibles errores ortograficos y gramaticales, las palabras deben existir en el diccionario de
+            la real academia española.
+            Debes de responder en formato JSON
+            tu tarea es corregirlos y retornar informacion de las solicitudes, tambien debes dar un porcentaje de acierto por el usuario,
+
+            Si no hay errores, debes retornar un mensaje de felicitaciones.
+            
+            Ejemplo de salida:
+            {
+                userScore: number,
+                errors: string[], //['error -> solucion']
+                message: string, //Usa emojis para felicitar al usuario
+            }
+      `,
+      },
       { role: 'user', content: prompt },
     ],
     model: 'gpt-3.5-turbo',
   });
 
-  console.log(completion);
-
-  return {
-    prompt,
-    completion,
-  };
+  return JSON.parse(completion.choices[0].message.content);
 };
